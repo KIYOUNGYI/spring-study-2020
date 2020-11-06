f55ad750203678d14ea997a4e29df35d598f30d0
KIYOUNGYI <yky2798@gmail.com>
2020-11-06 09:39:57 +0900

    일부분 의존관계 없애려고 리팩토링함(2)

--- src/main/java/hello/core/config/AppConfig.java
+++ src/main/java/hello/core/config/AppConfig.java
@@ -1,9 +1,13 @@
 package hello.core.config;
 
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
@@ -13,6 +17,15 @@ public class AppConfig {
 
   public MemberRepository memberRepository() {
     return new MemoryMemberRepository();
+  }
+
+  public DiscountPolicy discountPolicy() {
+    return new RateDiscountPolicy();
+  }
+
+
+  public OrderService orderService() {
+    return new OrderServiceImpl(memberRepository(), discountPolicy());
   }
 
 }

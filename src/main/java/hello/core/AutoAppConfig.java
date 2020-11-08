e2ca2d9eb142225aac86000b27d7280380e4ddc1
KIYOUNGYI <yky2798@gmail.com>
2020-11-08 16:35:04 +0900

    컴포넌트 스캔 공부

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
@@ -12,4 +15,21 @@ import org.springframework.context.annotation.FilterType;
 //실무에서는 이러진 않겠죠. 이렇게 한 것은 예제코드를 살리기 위해서!!!
 public class AutoAppConfig {
 
+  /**
+   * 아래 메소드 등록하고 AutoAppConfigTest.class 돌릴 때
+   * DEBUG org.springframework.beans.factory.support.DefaultListableBeanFactory -
+   * Overriding bean definition for bean 'memoryMemberRepository' with a different definition:
+   * replacing [Generic bean: class [hello.core.member.MemoryMemberRepository]; scope=singleton; abstract=false; lazyInit=null;
+   * =================================================================================================
+   * 스프링 부트 가동시
+   * The bean 'memoryMemberRepository', defined in class path resource
+   * [hello/core/AutoAppConfig.class], could not be registered.
+   * A bean with that name has already been defined in file
+   * [/Users/glowdayz/Documents/personal-study/core/out/production/classes/hello/core/member/MemoryMemberRepository.class]
+   * and overriding is disabled.
+   */
+  @Bean(name = "memoryMemberRepository")
+  public MemberRepository memberRepository() {
+    return new MemoryMemberRepository();
+  }
 }

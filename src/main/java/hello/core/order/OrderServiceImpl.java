a86c7120eca8294074ab2633f7a76de0463c5f34
KIYOUNGYI <yky2798@gmail.com>
2020-11-09 16:59:01 +0900

    Primary, Qualifier 등등 공부 (소스에는 Primary 적용)

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,6 +4,7 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,17 +13,17 @@ public class OrderServiceImpl implements OrderService {
   private MemberRepository memberRepository;
   private DiscountPolicy discountPolicy;
 
-  @Autowired
-  public void setMemberRepository(MemberRepository memberRepository) {
-    System.out.println("OrderServiceImpl.setMemberRepository");
-    this.memberRepository = memberRepository;
-  }
-
-  @Autowired
-  public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-    System.out.println("OrderServiceImpl.setDiscountPolicy");
-    this.discountPolicy = discountPolicy;
-  }
+//  @Autowired
+//  public void setMemberRepository(MemberRepository memberRepository) {
+//    System.out.println("OrderServiceImpl.setMemberRepository");
+//    this.memberRepository = memberRepository;
+//  }
+//
+//  @Autowired
+//  public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//    System.out.println("OrderServiceImpl.setDiscountPolicy");
+//    this.discountPolicy = discountPolicy;
+//  }
 
   @Autowired
   public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {

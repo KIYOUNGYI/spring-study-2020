533df0a862e4ed43c48ca8816e1b7d65bbf03fab
KIYOUNGYI <yky2798@gmail.com>
2020-11-18 09:48:18 +0900

    테스트 보완

--- src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -3,10 +3,19 @@ package hello.core.scan;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import hello.core.AutoAppConfig;
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 public class AutoAppConfigTest {
 
@@ -15,5 +24,39 @@ public class AutoAppConfigTest {
     ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
     MemberService memberService = ac.getBean(MemberService.class);
     assertThat(memberService).isInstanceOf(MemberService.class);
+  }
+
+  @Test
+  @DisplayName("여러 번 sout 찍은게 출력되어야 한다")
+  void baseScan2() {
+    ApplicationContext ac = new AnnotationConfigApplicationContext(LocalAppConfig.class);
+    LocalAppConfig bean = ac.getBean(LocalAppConfig.class);
+    System.out.println("bean = " + bean);
+  }
+
+  static class LocalAppConfig {
+
+    @Bean
+    public MemberService memberService() {
+      System.out.println("call AppConfig.memberService");
+      return new MemberServiceImpl(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+      System.out.println("call AppConfig.memberRepository");
+      return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+      return new RateDiscountPolicy();
+    }
+
+    @Bean
+    public OrderService orderService() {
+      System.out.println("call AppConfig.orderService");
+      return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
   }
 }
